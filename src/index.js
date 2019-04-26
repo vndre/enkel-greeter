b29ec101f93d63b300800b80f1ae1b694650894a
bringmethecode <dev@ansky.sh>
2019-04-26 10:48:52 -0600

    Add fonts style and lightDM mock to index

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import Login from './components/Login.svelte'
+import './fonts.css'
+
+if (process.env.NODE_ENV === 'development') {
+  require('./utils/lightDM.dev')
+}
 
 const app = new Login({
-  target: document.querySelector('main'),
-  data: {
-    quotes: []
-  },
+  target: document.querySelector('main')
 })
