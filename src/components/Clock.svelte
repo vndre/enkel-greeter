90652b25ce0f81d6aa16f5d2ed61373bba2c3f64
bringmethecode <dev@ansky.sh>
2019-04-27 19:00:52 -0600

    Create Clock and PowerOptions component

---
+++ src/components/Clock.svelte
@@ -0,0 +1,30 @@
+<script>
+  import { onMount, onDestroy } from 'svelte'
+
+  let time = new Date()
+  let interval = undefined
+  $: hours = time.getHours()
+  $: minutes = (time.getMinutes() < 10)
+    ? `0${time.getMinutes()}`
+    : time.getMinutes()
+
+  onMount(() => {
+    interval = setInterval(() => {
+      time = new Date()
+    }, 3000)
+  })
+
+  onDestroy(() => clearInterval(this.interval))
+</script>
+
+<style>
+  .clock {
+    display: flex;
+  }
+</style>
+
+<div class='clock'>
+  <span>{hours}</span>
+  <span>:</span>
+  <span>{minutes}</span>
+</div>
--- src/components/PowerOptions.svelte
+++ src/components/PowerOptions.svelte
@@ -1,5 +1,6 @@
 <script>
   import { onMount } from 'svelte'
+  import Clock from './Clock.svelte'
 
   let userOptions = {
     hibernate: lightdm.can_hibernate,
@@ -7,9 +8,38 @@
     shutdown: lightdm.can_shutdown,
     suspend: lightdm.can_suspend
   }
-
 </script>
 
+<style>
+  div {
+    display: flex;
+    color: white;
+    font-family: monospace;
+    position: absolute;
+    bottom: 0;
+    right: 0;
+    padding: 0 20px 20px 0;
+    user-select: none;
+  }
+  .action {
+    cursor: pointer;
+    opacity: 0.5;
+    transition: all 300ms ease-in-out;
+  }
+  .action:hover {
+    opacity: 1;
+  }
+  .divider {
+    margin: 0 10px;
+  }
+</style>
+
 <div>
-  <span>heloooo</span>
+  {#each Object.keys(userOptions) as option, index}
+    {#if userOptions[option]}
+      <span class='action'>{Object.keys(userOptions)[index]}</span>
+      <span class='divider'>—</span>
+    {/if}
+  {/each}
+  <Clock />
 </div>
