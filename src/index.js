109767ad42d520bec3c0d92ecd3938bd7bcc4bb7
bringmethecode <dev@ansky.sh>
2019-04-29 10:11:20 -0600

    Fix require file name

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import Login from './components/Login.svelte'
 import './fonts.css'
 
 if (process.env.NODE_ENV === 'development') {
-  require('./utils/lightDM.dev')
+  require('./utils/lightDM.dev.js')
 }
 
 const app = new Login({
